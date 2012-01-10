be4300319bf5088ba85d1ad922bd4012385a5a3d
Jeremy Bondeson <jbondeson@gmail.com>
2012-01-10 12:53:58 -0700

    no really, adding the file this time

---
+++ jvm/data/util/EditContext.java
@@ -0,0 +1,28 @@
+package data.util;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class EditContext {
+    private final AtomicReference<Thread> _ctx;
+
+    public EditContext() {
+        this(new AtomicReference<Thread>(Thread.currentThread()));
+    }
+
+    public EditContext(AtomicReference<Thread> ctx) {
+        this._ctx = ctx;
+    }
+
+    public void persist() {
+        this._ctx.set(null);
+    }
+
+    public void ensureEditable() {
+        Thread owner = this._ctx.get();
+        if(owner == Thread.currentThread())
+            return;
+        if(owner != null)
+            throw new IllegalAccessError("Edit attempted by non-owner thread");
+        throw new IllegalAccessError("Edit attempted after persist! call");
+    }
+}
