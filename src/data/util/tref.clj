bbcab8c06ff52c5adf0d2693b11ca93262285eb9
Jeremy Bondeson <jbondeson@gmail.com>
2012-01-15 19:34:29 -0700

    added persist call for edit context.

--- src/data/util/tref.clj
+++ src/data/util/tref.clj
@@ -11,20 +11,24 @@
   []
   (EditContext.))
 
-(defn thread-bound-ref ^data.util.ThreadBoundRef
+(defn persist!
+  [^EditContext edit]
+  (.persist edit))
+
+(defn thread-bound-ref ^ThreadBoundRef
   ^{:inline (fn [v e] `(ThreadBoundRef. v e))
     :inline-arities #{2}}
   ([value]
      (ThreadBoundRef. value))
-  ([value ^data.util.EditContext edit]
+  ([value ^EditContext edit]
      (ThreadBoundRef. value edit)))
 
 (defn set!
-  ^{:inline (fn [^data.util.ThreadBoundRef r v] `(.set r v))
+  ^{:inline (fn [^ThreadBoundRef r v] `(.set r v))
     :inline-arities #{2}}
-  [^data.util.ThreadBoundRef ref value]
+  [^ThreadBoundRef ref value]
   (.set ref value))
 
 (defn editable?
-  [^data.util.ThreadBoundRef ref]
+  [^ThreadBoundRef ref]
   (.isEditable ref))
